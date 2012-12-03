3b42058ac95a72db111446a7fe35d30d126ca98d
Ajay K <kulkarni.ajay@gmail.com>
2012-12-02 19:29:00 -0500

    first!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Callmom::Application.config.secret_token = '106b7e9afc89cec10f7a260ef9e7b76d1d7f24059fbb7ebb7782a37523557b827b99eeb6ae34c68c9c71b272f442c9775877ca88d0359b344180b84d6b05f0f8'
