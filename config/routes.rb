446bb572b53701bcb57f3c387e8715a39546c4f3
acoustik <ajay@acoustik.org>
2013-02-21 13:00:36 -0500

    more REORG. starting to make this about more than mom.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Callmom::Application.routes.draw do
-  match 'mom/call_ended' => 'mom#call_ended'
-  match 'mom/logz' => 'mom#logs'
+  match 'call/call_ended' => 'call#call_ended'
+  match 'call/logz' => 'call#logs'
 
   match 'test/call' => 'test#call'
   match 'test/create_dummy_log' => 'test#create_dummy_log'
 
-  resources :mom
+  resources :call
   resources :grade
   resources :test
 
