3d6017470d357c4e9531800cd925620c815a7f21
acoustik <ajay@acoustik.org>
2013-04-06 09:34:05 -0400

    now buys a new system number from twilio

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,14 +1,6 @@
 class RegisterController < ApplicationController
   def index
-    
-    @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
-    @client.account.sms.messages.create(
-                                        :from => '+19177192233',
-                                        :to => '+19175731568',
-                                        :body => 'Heyo!'
-                                        )
-
-    render :text => 'OK'
+    render :text => get_twilio_system_number
   end
 
   def create
@@ -16,7 +8,17 @@ class RegisterController < ApplicationController
   end
 
   def get_twilio_system_number
-    render :text => 'OK'    
+    @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+    @numbers = @client.account.available_phone_numbers.get('US').local.list(:contains => 'MOM')
+    @number = @numbers[0].phone_number
+    @client.account.incoming_phone_numbers.create(:phone_number => @number)
+
+    @client.account.incoming_phone_numbers.list({:phone_number => @number}).each do |registered_number|
+      registered_number.update.update(:voice_url => ENV['HEYMOM_HOST'] + "/call",
+                                      :status_callback => ENV['HEYMOM_HOST'] + "/call/call_ended")
+    end
+
+    return @number
   end
 
 end
