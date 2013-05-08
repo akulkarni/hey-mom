838324bd7e6e5493bb64b601807751839fdbf98b
satish-zol <zsatish201@gmail.com>
2013-05-08 16:54:48 +0530

    added score calcuation logic in model

--- app/models/phone_call.rb
+++ app/models/phone_call.rb
@@ -1,4 +1,6 @@
 class PhoneCall < ActiveRecord::Base
+  
+
   validates_uniqueness_of :call_sid, :case_sensitive => true
   validates :duration, :response_time, :numericality => { :only_integer => true }, :allow_nil => true
 
@@ -25,6 +27,26 @@ class PhoneCall < ActiveRecord::Base
       direction = 'outbound'
     end
     direction
+  end
+
+  #score calculation
+
+  
+  def self.score_total_outbound_calls(user)
+    return where("created_at > '%s' and system_number = '%s' and inbound = false", Time.now()-604800, user.system_number).count
+  end
+
+  def self.score_total_seconds(user)
+    return where("created_at > '%s' and system_number = '%s' and missed_call is not true", Time.now()-604800, user.system_number).sum('duration')
+  end
+
+  def self.score_average_response_time(user)
+    phone_calls = where("created_at > '%s' and system_number = '%s' and inbound = true and missed_call = true", Time.now()-604800, user.system_number)
+    sum = 0
+    for pc in phone_calls
+        pc.response_time.nil? ? (sum += (Time.now() - pc.created_at)) : (sum += pc.response_time)
+    end
+    sum == 0 ? (return 86400) : (return sum / phone_calls.count)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
-  attr_accessible :contact_name, :contact_phone_number, :name, :phone_number, :system_number
+  
+  attr_accessor :current_call_score
+
+  attr_accessible :contact_name, :contact_phone_number, :name, :phone_number, :system_number, :current_call_score
+ 
+
+	  
+
+  def current_score
+  	total_outbound = PhoneCall.score_total_outbound_calls(self)
+    total_seconds = PhoneCall.score_total_seconds(self)
+    average_response_time = PhoneCall.score_average_response_time(self)
+    self.current_call_score = total_outbound/3.to_f + total_seconds/3600.to_f + 86400/average_response_time.to_f
+  end
+
+
 end
